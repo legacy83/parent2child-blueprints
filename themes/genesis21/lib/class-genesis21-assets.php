bfe75099c3c77dc86a4e3a56aed6082214a9b880
Thiago Senna <trsenna@gmail.com>
2015-07-19 12:33:39 -0300

    enhanced the mobile-first styles

--- themes/genesis21/lib/class-genesis21-assets.php
+++ themes/genesis21/lib/class-genesis21-assets.php
@@ -48,6 +48,10 @@ final class Genesis21_Assets
         $child_dir_uri = trailingslashit( get_stylesheet_directory_uri() );
         wp_enqueue_style( 'genesis21-normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css' );
         wp_enqueue_style( 'genesis21-theme', "{$child_dir_uri}lib/assets/css/theme.css" );
+        wp_enqueue_style( 'genesis21-theme-gt568px', "{$child_dir_uri}lib/assets/css/theme-gt568px.css", array(), FALSE, 'only screen and (min-width: 35.5em)' );
+        wp_enqueue_style( 'genesis21-theme-gt768px', "{$child_dir_uri}lib/assets/css/theme-gt768px.css", array(), FALSE, 'only screen and (min-width: 48em)' );
+        wp_enqueue_style( 'genesis21-theme-gt1024px', "{$child_dir_uri}lib/assets/css/theme-gt1024px.css", array(), FALSE, 'only screen and (min-width: 64em)' );
+        wp_enqueue_style( 'genesis21-theme-gt1280px', "{$child_dir_uri}lib/assets/css/theme-gt1280px.css", array(), FALSE, 'only screen and (min-width: 80em)' );
         wp_enqueue_style( 'genesis21' );
     }
 
